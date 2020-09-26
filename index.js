3516d01344d676e02664afbdc7db1a674e7504fc
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-26 23:21:01 +0530

    add user model and connect to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const authRoutes = require("./routes/auth");
 
 const app = express();
 
+const keys = require("./config/keys");
 const PORT = 5000;
 
 app.use(authRoutes);
@@ -11,6 +14,15 @@ app.get("/", (req, res, next) => {
   res.send({ hello: "there" });
 });
 
-app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
-});
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`app running on port ${PORT}`);
+    });
+  });
