b171bbe89fb87249ebfb5245996422d2dea51373
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-29 01:14:01 +0530

    console log req.headers to display cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/auth");
 
@@ -8,12 +9,31 @@ const app = express();
 const keys = require("./config/keys");
 const PORT = 5000;
 
-// app.set("trust proxy", true);
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // * for all domains
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// app.use(cookieParser);
 
 app.use(authRoutes);
 
 app.get("/", (req, res, next) => {
   res.send({ hello: "there" });
+});
+
+app.get("/api/ping", (req, res, next) => {
+  console.log("/api/ping");
+  res.json({ ping: "pong" });
+  console.log(req.cookies);
+  console.log(req.headers.cookie);
 });
 
 mongoose
