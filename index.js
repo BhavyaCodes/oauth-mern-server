6084c445b3905cd6817b7907fc2f13ec519e9a53
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-30 17:29:04 +0530

    add .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+// const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -11,10 +13,11 @@ const app = express();
 const keys = require("./config/keys");
 const PORT = 5000;
 
+app.set("trust proxy", true);
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // * for all domains
+  res.setHeader("Access-Control-Allow-Origin", "null");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
@@ -23,6 +26,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+// app.use(
+//   cors({
+//     origin: false,
+//   })
+// );
 
 app.use(authRoutes);
 
