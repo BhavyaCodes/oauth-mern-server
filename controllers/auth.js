ad2bb5e03044ac5cd1ad74b879294fa66f51f3b2
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-02 02:39:05 +0530

    change redirect url to relative

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,7 +38,7 @@ exports.getGoogleAuthCallback = async (req, res, next) => {
     });
     res.cookie("isLoggedIn", "1", { maxAge: 60000 });
     res.cookie("asd", "dfgh", { SameSite: "Lax" });
-    return res.redirect(process.env.CLIENT_URL);
+    return res.redirect("/");
   }
   const user = new User({
     googleId: decoded.payload.sub,
@@ -60,7 +60,7 @@ exports.getGoogleAuthCallback = async (req, res, next) => {
   });
   res.cookie("asd", "dfgh", { SameSite: "Lax" });
   res.cookie("isLoggedIn", "1", { maxAge: 60000 });
-  res.redirect(process.env.CLIENT_URL);
+  res.redirect("/");
 };
 
 exports.getLogout = (req, res, next) => {
